b4b3c91123fac0a80b20db8e24af2617afecce7a
pma365 <appleamirbahador@gmail.com>
2025-12-30 00:45:22 +0330

    learned how the costum roundTo function working

--- chapter_03_functions/README.md
+++ chapter_03_functions/README.md
@@ -1,1 +1,34 @@
 # Functions
+
+#### Are there any other function syntaxes without braces?
+
+JavaScript has only these three ways to define functions:
+
+Function Declaration
+
+```js
+function f() {}
+```
+
+Function Expression const
+
+```js
+f = function () {};
+```
+
+Arrow Function
+
+```js
+// prettier-ignore
+const square = x => x * x;
+const square = (x) => x * x;
+```
+
+Arrow Function with Braces
+
+```js
+const square = (x) => {
+	return x * x;
+};
+```
+Functions that don’t have a return statement at all, such as makeNoise, similarly return undefined
---
+++ chapter_03_functions/examples.js
@@ -0,0 +1,32 @@
+const square = (x) => x * x;
+square(2);
+
+const roundTo = function (n, step) {
+	let remainder = n % step;
+	return n - remainder + (remainder < step / 2 ? 0 : step);
+};
+console.log(roundTo(13, 10));
+// How they found this formula lets test some numbers ourself:
+// 13 % 10 = 3
+// means 13 is +3 more than our nearest step (10)
+// also its less than the middle of the distance between 10 and 20 meaning <15
+// so we should round it down
+// if it was more than the middle of the distance between 10 and 20 meaning >15
+// we should round it up to the next nearly step
+// if(remainder < step/2 ? roundDown : roundUp)
+// roundDown: n - remainder
+// n = 17
+// remainder = 17 % 10 = 7
+// roundUp: n - remainder = nearest down step
+// + step => next nearest step
+// formula = n - remainder + ( remainder < step/2 ? 0 : step)
+// n = 9 step=10
+// remainder = 9
+// 9>5 => n - remainder + step = 10
+
+// Functions that don’t have a return statement at all,
+//  such as makeNoise, similarly return undefined.
+const makeNoise = function () {
+	console.log("Pling!");
+};
+console.log(makeNoise()); // -> undefined
