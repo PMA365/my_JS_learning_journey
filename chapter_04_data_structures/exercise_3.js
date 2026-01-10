fffa8aa359c3193fb210de7ce2c903647f9cd82f
pma365 <appleamirbahador@gmail.com>
2026-01-10 20:05:01 +0330

    exercise A List done successfully

--- chapter_04_data_structures/exercise_3.js
+++ chapter_04_data_structures/exercise_3.js
@@ -1,10 +1,59 @@
 const arrayToList = (inputArray) => {
-	let list = {};
-	let i = 0;
-	let rest = {};
-	for (let i = inputArray.lenght - 1; i >= 0; i--) {
-		list.rest = {};
-		rest = { value: inputArray[i], rest: null };
+	let isOldObjectTurn = true;
+	let oldObject = {};
+	let newObject = {};
+	for (let i = inputArray.length - 1; i >= 0; i--) {
+		if (isOldObjectTurn) {
+			if (i == inputArray.length - 1) {
+				oldObject.value = inputArray[i];
+				oldObject.rest = null;
+			} else {
+				oldObject.value = inputArray[i];
+				oldObject.rest = newObject;
+				newObject = {};
+			}
+			isOldObjectTurn = false;
+		} else {
+			newObject.value = inputArray[i];
+			newObject.rest = oldObject;
+			oldObject = {};
+			isOldObjectTurn = true;
+		}
 	}
+	return isOldObjectTurn ? newObject : oldObject;
 };
-arrayToList([1, 2, 3]);
+let newList = arrayToList([1, 2, 3]);
+console.log(newList);
+
+const listToArray = (inputList) => {
+	let array = [];
+	const checkCurrentObjectRest = (inputObject) => {
+		array[inputObject.value - 1] = inputObject.value;
+		return inputObject.rest ? checkCurrentObjectRest(inputObject.rest) : array;
+	};
+	return checkCurrentObjectRest(inputList);
+};
+console.log(listToArray(newList));
+
+const prepend = (inputElement, inputList) => {
+	let newList = {};
+	newList.value = inputElement;
+	newList.rest = inputList;
+	return newList;
+};
+console.log(prepend(0, newList));
+
+const nth = (inputElementNumber, inputList) => {
+	const searchingForTheElement = (inputList) => {
+		if (inputElementNumber + 1 == inputList.value) {
+			return inputList;
+			//finished
+		} else if (inputList.rest == null) {
+			return undefined;
+		} else {
+			return searchingForTheElement(inputList.rest);
+		}
+	};
+	return searchingForTheElement(inputList);
+};
+console.log(nth(1, newList));
