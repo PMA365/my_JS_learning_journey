1298566c773b3558286dc7521a3c83794f9ff1a4
pma365 <appleamirbahador@gmail.com>
2026-09-21 23:45:11 +0330

    exercise 7

--- reviewing_with_AI_exercises/chapter_01_02/exercise_7.js
+++ reviewing_with_AI_exercises/chapter_01_02/exercise_7.js
@@ -1,1 +1,7 @@
-
+// EXERCISE 7: TYPE COERCION OBSERVATIONS
+// Task: Predict the output of these expressions and explain why:
+// 1. "5" - 3
+// 2. "5" + 3
+// 3. 8 * null
+// 4. "5" == 5
+// 5. "5" === 5
