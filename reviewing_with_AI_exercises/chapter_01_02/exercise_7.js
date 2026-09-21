86682d81a0c43de88d9b59920430a0b131f9b9cb
pma365 <appleamirbahador@gmail.com>
2026-09-21 23:57:31 +0330

    exercise 7 done :))

--- reviewing_with_AI_exercises/chapter_01_02/exercise_7.js
+++ reviewing_with_AI_exercises/chapter_01_02/exercise_7.js
@@ -5,3 +5,17 @@
 // 3. 8 * null
 // 4. "5" == 5
 // 5. "5" === 5
+
+//1/ will be 2 bc the "5" will be converted to the number
+//2/ will be 8 bc the "5" will be converted to the number
+//Note: I was wrong it will be 53 bc it concat it :))
+//3/ 8 * null = null  I think every think x null will be null
+//Note: no i was wrong it will be 0
+//Note: bc When you use an arithmetic operator like multiplication (*),
+//Note: JavaScript expects both sides to be numbers.
+//Note: If one of them is not a number, JavaScript automatically converts (coerces) it behind the scenes before doing the math.
+
+//4/ "5" == 5  will be True
+//5/ "5" === 5 will be False bc the type of the "5" will be string
+
+console.log("5" - 3);
