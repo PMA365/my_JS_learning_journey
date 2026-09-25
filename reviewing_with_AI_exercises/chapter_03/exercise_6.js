8c4ec17102d772af138abd6eee70ef5df7984244
pma365 <appleamirbahador@gmail.com>
2026-09-25 23:43:56 +0330

    exercise 6 question added

---
+++ reviewing_with_AI_exercises/chapter_03/exercise_6.js
@@ -0,0 +1,5 @@
+// Higher-Order: map
+
+// Write a function called myMap that takes an array and a function as arguments. It should return a new array containing the results of applying the function to each element of the original array.
+
+// Do not use the built-in Array.map method - implement it yourself using a loop.
