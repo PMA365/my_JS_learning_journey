2c5dafd6dd06c3c0afe6ad55c6414d7094976776
pma365 <appleamirbahador@gmail.com>
2026-09-25 23:29:48 +0330

    lil update

---
+++ reviewing_with_AI_exercises/chapter_03/exercise_5.js
@@ -0,0 +1,5 @@
+// Closure Practice
+
+// Write a function called makeCounter that returns an object with two methods: increment() and get().
+
+// Each call to makeCounter should create a new, independent counter. The increment() method should increase the counter by 1 and return the new value. The get() method should return the current value.
